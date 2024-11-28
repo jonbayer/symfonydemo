909e5c39c44b8d1e9c539f3d41014b3cf11d420a
Jonathan Bayer <jonbayer@gmail.com>
2024-11-28 18:52:49 -0400

    looking less ugly

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["symfonydemo_173282*"]
+    values = ["symfonydemo_*"]
   }
 
   filter {
@@ -52,14 +52,6 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-//  user_data = <<-EOF
-//              #!/bin/bash
-//              apt-get update
-//              apt-get install -y apache2
-//              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-//              echo "Hello World" > /var/www/html/index.html
-//              systemctl restart apache2
-//              EOF
 }
 
 resource "aws_security_group" "web-sg" {
